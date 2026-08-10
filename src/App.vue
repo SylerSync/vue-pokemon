7c192009be833e03d767b832567fcb8a5379b784
brendan-sync <brendan.klostermann@syncbak.com>
2026-08-10 13:54:34 -0500

    added settingsStore.js
    created mute toggle on app.vue
    created logic to handle muting in WildPokemon.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 import Tabs from 'primevue/tabs';
 import TabList from 'primevue/tablist';
 import Tab from 'primevue/tab';
+import Checkbox from "primevue/checkbox";
+import { useSettingsStore } from '@/stores/settingsStore';
+
+const settingsStore = useSettingsStore()
+
 </script>
 
 <template>
@@ -11,8 +16,14 @@ import Tab from 'primevue/tab';
             <Tab class="custom-tab" value="/wildPokemon" as="router-link" to="/wildPokemon">Wild Pokemon</Tab>
             <Tab class="custom-tab" value="/pokebox" as="router-link" to="/pokebox">PokeBox</Tab>
             <Tab class="custom-tab" value="/wishList" as="router-link" to="/wishList">WishList</Tab>
+            <div class="settings-toggle">
+                <Checkbox v-model="settingsStore.muteAudio" :binary="true" inputId="muteAudio" />
+                <label for="muteAudio" class="toggle-label">Mute Audio</label>
+            </div>
         </TabList>
   </Tabs>
+
+
   <router-view />
 </template>
 
@@ -34,4 +45,26 @@ import Tab from 'primevue/tab';
          border-color: CanvasText;
 
     }
+
+    :deep(.p-tablist-tab-list) {
+    display: flex;
+    align-items: center;
+    width: 100%;
+}
+
+
+.settings-toggle {
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 0 1rem;
+}
+
+.toggle-label {
+    cursor: pointer;
+    font-size: 0.9rem;
+    user-select: none;
+    color: CanvasText;
+}
 </style>
